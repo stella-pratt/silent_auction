05229f144e6f333bedbffc3883de64d102a0c35a
Sebastian <82354363+seb420@users.noreply.github.com>
2023-02-13 09:42:26 +1300

    main part made

--- silent_auction.py
+++ silent_auction.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bids = {}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def try_float(number):
+    """
+    This function will try to convert the input to a float.
+    If it is not a float it will return False.
+    """
+    try:
+        float(number)
+        return True
+    except ValueError:
+        return False
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def silent_bid():
+    """collect the silent bids"""
+    name = input("What is your name? ").title()
+    if name == "F":
+        return "F"
+    bid = input("What is your bid? ")
+    while not try_float(bid) or float(bid) < 0:
+        bid = input("What is your bid? ")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+def main():
+    """
+    run the main part of the code
+    """
+    reserve = input("What is the reserve price? ")
+    while not try_float(reserve) or float(reserve) < 0:
+        reserve = input("What is the reserve price? ")
+    print("Auction has started")
+    fail_test = silent_bid()
+    while fail_test != "F":
+        fail_test = silent_bid()
+
+
+main()
